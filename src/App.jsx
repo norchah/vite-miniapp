d6c8ef1cb3f9f54c3bd33ba8f8df7b51b81ecfeb
norchah <norchah@mail.ru>
2025-09-18 10:43:20 +0200

    dev dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Keys from "./pages/keys.jsx";
 import Payments from "./pages/payments.jsx";
 import BottomNav from "./components/common/bottomNav.jsx";
-import {useTG} from "./hooks/useTG.js";
 import TheHeader from "./components/ui/layout/TheHeader.jsx";
-import {useEffect, useState} from "react";
-import useMiniAppInit from "./hooks/useMiniAppInit.js";
+import {useMiniAppInit, useMiniApp} from "./hooks/useMiniAppInit.js";
 
 
 export default function App() {
-  const tg = useTG()
+  const {viewport} = useMiniApp();
+  useMiniAppInit()
 
-  const {viewport} = useMiniAppInit(tg)
-
-useEffect(() => {
-    if (!tg) {
-      // return
-    }
-    // setPlatform(tg.platform)
-    // setSource(tg.source)
-  }, [tg]);
-
-
-  console.log(tg.source)
   return (
     <div className="bg-gray-800 text-white h-screen flex flex-col items-center">
       <TheHeader/>
--- src/hooks/useMiniAppInit.js
+++ src/hooks/useMiniAppInit.js
@@ -1,19 +1,25 @@
 import {useEffect, useState} from 'react';
+import {useTG} from "./useTG.js";
 
-export default function useMiniAppInit(tg) {
-  const [viewport, setViewport] = useState(0);
+export function useMiniAppInit() {
+  const tg = useTG()
   useEffect(() => {
     if (!tg) return null;
     if (tg.platform !== 'tdesktop') {
       tg.disableVerticalSwipes();
       tg.lockOrientation();
       tg.requestFullscreen();
-      setViewport(tg.viewportHeight);
     }
 
   }, [tg]);
-  return {viewport}
 }
 
-
-// function isT
+export function useMiniApp() {
+  const tg = useTG()
+  const [viewport, setViewport] = useState(0);
+  useEffect(() => {
+    if (!tg) return null;
+    setViewport(tg.viewportHeight);
+  }, [tg]);
+  return {viewport}
+}
