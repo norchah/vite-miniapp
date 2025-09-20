0f43a6bef3d58aec4f57b2e7130ad1c1f304fc4e
norchah <norchah@mail.ru>
2025-09-20 19:45:35 +0200

    dev dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import TheHeader from "./components/ui/layout/TheHeader.jsx";
 import {useMiniAppInit} from "./hooks/useMiniAppInit.js";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {renderPage} from "./utils/renderPage.js";
 import {UserApi} from "./api/userApi.js";
 import {useTG} from "./hooks/useTG.js";
@@ -12,10 +12,15 @@ import TheFooter from "./components/ui/layout/bottomNav.jsx";
 export default function App() {
   const [page, setPage] = useState('home');
   const [user, setUser] = useState(null);
-  const safe = useMiniAppInit()
+  const [stayleMain, setStayleMain] = useState(0);
+
+  useEffect(() => {
+    setStayleMain(layoutConfig.safeAreaTop);
+  }, [layoutConfig.safeAreaTop]);
+
   const tg = useTG()
 
-
+  useMiniAppInit()
   const handleClick = async () => {
     const api = new UserApi();
     const res = await api.login(tg.initData)
@@ -25,17 +30,16 @@ export default function App() {
 
   return (
     <div className={`bg-slate-800 text-white h-screen flex flex-col items-center`}
-         style={{paddingTop: `${layoutConfig.safeAreaTop}px`}}
+         style={{paddingTop: `${stayleMain}px`}}
     >
       <TheHeader/>
       <button onClick={handleClick} className='bg-sky-600 hover:cursor-pointer active:bg-sky-800 p-3 mb-2'>Жать и
         смотреь
       </button>
-      {/*{AddToHomeButton()}*/}
       <p>{user && user.username}</p>
 
-      <p>safe device top: {layoutConfig.safeAreaTop} пикселей</p>
-      <p>safe device bottom: {layoutConfig.safeAreaBottom} пикселей</p>
+      <p>safe device top: {layoutConfig.safeAreaTop} px</p>
+      <p>safe device bottom: {layoutConfig.safeAreaBottom} px</p>
       <div className="flex-1 w-full">{renderPage(page)}</div>
       {user && user.tg_id === 116627792 ? <AdminPage/> : null}
       <TheFooter setPage={setPage} currentPage={page}/>
