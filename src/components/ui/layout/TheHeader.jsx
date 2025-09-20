aa2a96a46177d6e6d5140b5d204cf16691d6be81
norchah <norchah@mail.ru>
2025-09-20 17:27:18 +0200

    dev dev

--- src/components/ui/layout/TheHeader.jsx
+++ src/components/ui/layout/TheHeader.jsx
@@ -5,7 +5,7 @@ import {siteConfig} from "../../../configs/siteConfig.js";
 
 export default function TheHeader() {
   return (
-    <header className={`${layoutConfig.flexColItemsJustify} pt-15`}>
+    <header className={`${layoutConfig.flexColItemsJustify}`}>
       <img src="/logo-free-bg.svg" alt="logo" width={80} height={80}/>
       <h1 className='text-bold text-xl'>KeyCraft</h1>
       <p className='max-w-[320px] text-center text-gray-200'>{siteConfig.mainDescription}</p>
