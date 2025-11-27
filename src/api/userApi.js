a1cfb412b91b49c18363b04dd7602a8e62dec076
norchah <norchah@mail.ru>
2025-11-27 23:33:14 +0200

    commit first after long time

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,7 +5,7 @@ import camelcaseKeys from "camelcase-keys";
 export class UserApi {
   constructor() {
     this.api = axios.create({
-      baseURL: 'https://test.norchah.ru/users',
+      baseURL: 'https://test.norchah.ru/test/users/',
       withCredentials: true,
       headers: {
         'Content-Type': 'application/json',
@@ -16,7 +16,7 @@ export class UserApi {
 
   async login(initData) {
     console.log(initData);
-    const res = await this.api.post('/login', {init_data: initData});
+    const res = await this.api.post('/login_miniapp', {init_data: initData});
     return camelcaseKeys(res.data, {deep: true});
   }
 
