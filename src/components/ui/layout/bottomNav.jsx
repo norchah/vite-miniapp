6835d1998e6fbc5c90efc90e375257209d3aa446
norchah <norchah@mail.ru>
2025-11-30 22:05:36 +0200

    fix

--- src/components/ui/layout/bottomNav.jsx
+++ src/components/ui/layout/bottomNav.jsx
@@ -2,7 +2,7 @@ import { siteConfig } from "../../../configs/siteConfig.js";
 
 export default function TheFooter({ setPage, currentPage, safeZoneBottom }) {
   return (
-    <div className="fixed bottom-0 left-0 right-0 px-2 pb-2 outline"
+    <div className="fixed bottom-0 left-0 right-0 px-2 pb-2 outline outline-2"
          style={{ marginBottom: `${safeZoneBottom}px` }}>
       <nav
         className="
