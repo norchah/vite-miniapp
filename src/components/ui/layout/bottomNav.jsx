98a78bba11f2fa86fdb687926395232a5c33bf47
norchah <norchah@mail.ru>
2025-11-30 15:32:05 +0200

    fix

--- src/components/ui/layout/bottomNav.jsx
+++ src/components/ui/layout/bottomNav.jsx
@@ -3,7 +3,7 @@ import {siteConfig} from "../../../configs/siteConfig.js";
 
 export default function TheFooter({setPage, currentPage, safeBottom}) {
   return (
-    <nav className="fixed bottom-0 outline-auto h-21 bg-gray-800 border-t-1 border-gray-600 pt-2"
+    <nav className="fixed bottom-0 mb-4 h-21 bg-gray-800 pt-2"
          style={{paddingBottom: `${safeBottom}px`}}>
       <ul className="flex flex-row justify-around w-full h-full">
         {siteConfig.bottomNavMenu.map((item) => {
