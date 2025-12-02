dbc60644f382aef6a13e1683d48df09c25e77599
norchah <norchah@mail.ru>
2025-12-02 17:58:33 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -36,16 +36,16 @@ export default function App() {
 
       {/* Карточки */}
       <ul className="flex flex-col w-full mt-6">
-        {siteConfig.navMenu.map((card) => (
-          <li
-            key={card.id}
-            className="w-full h-10 bg-slate-700 flex items-center justify-center text-xs font-medium first:rounded-t-lg mb-[2px] last:rounded-b-lg"
-          >
-            <button onClick={() => console.log(card.href)} className="w-full h-full">
-              {card.name}
-            </button>
-          </li>
-        ))}
+        {/*{siteConfig.navMenu.map((card) => (*/}
+        {/*  <li*/}
+        {/*    key={card.id}*/}
+        {/*    className="w-full h-10 bg-slate-700 flex items-center justify-center text-xs font-medium first:rounded-t-lg mb-[2px] last:rounded-b-lg"*/}
+        {/*  >*/}
+        {/*    <button onClick={() => console.log(card.href)} className="w-full h-full">*/}
+        {/*      {card.name}*/}
+        {/*    </button>*/}
+        {/*  </li>*/}
+        {/*))}*/}
       </ul>
 
       {/* Страница админа */}
